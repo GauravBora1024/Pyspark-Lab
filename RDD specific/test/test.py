e3b733a0d7413cce49b4688f271fed64792b26bf
Gaurav <gaurav.bora973@gmail.com>
2025-08-02 02:03:33 +0530

    eod 1 aug

--- RDD specific/test/test.py
+++ RDD specific/test/test.py
@@ -1,18 +1,5 @@
-from pyspark import SparkContext, SparkConf
-import re
-sc = SparkContext(conf=SparkConf().setMaster("local").setAppName("word"))
-
-def normalizeWords(text):
-    return re.compile(r'\W+', re.UNICODE).split(text.lower())
-
-input = sc.textFile("RDD specific/test/book.txt")
-
-words = input.flatMap(normalizeWords)
-words_count = words.map(lambda x: (x,1)).reduceByKey(lambda x,y : x+y)
-Reversed = words_count.map(lambda xy : (xy[1],xy[0])).sortByKey(ascending = False)
-# wordsCount = words.countByValue()
-
-for i,j in Reversed.collect():
-    cleanword = j.encode('ascii', 'ignore').decode('utf-8')
-    if cleanword:
-        print(j,i)
+string = "the fox is  an animal"
+new_str = ""
+for i in range(len(string)-1,-1,-1):
+    new_str+= string[i]
+print(new_str)
